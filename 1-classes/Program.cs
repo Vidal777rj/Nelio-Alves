6dc6dc3d5564f708aa23468dfdfcec6c6d008354
Vidal777rj <michelvidal65@outlook.com>
2025-04-27 16:45:07 -0300

    iniciando primeiro modulo

---
+++ 1-classes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+/*
+---------- Sem ORIETAÇÃO A OBJETOS ----------
+double xA, xB, xC, yA, yB, yC;
+
+Console.WriteLine("Entre com as medidas da triângulo X:");
+
+xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+Console.WriteLine("Entre com as medidas da triângulo Y:");
+yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double p = (xA + xB + xC) / 2.0;
+double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+p = (yA + yB + yC) / 2.0;
+double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+Console.WriteLine($"Área de X =  {areaX.ToString("F4", CultureInfo.InvariantCulture)}");
+Console.WriteLine($"Área de Y =  {areaY.ToString("F4", CultureInfo.InvariantCulture)}");
+
+if (areaX > areaY)
+{
+    Console.WriteLine("A maior área: X");
+}else{
+    System.Console.WriteLine("Maior área: Y");
+}
+*/
