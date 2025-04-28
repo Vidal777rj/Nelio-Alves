77f6b987a12c84b1095f921962a7498e26110592
Vidal777rj <michelvidal65@outlook.com>
2025-04-27 22:54:48 -0300

    orientação objetos

--- 1-classes/Program.cs
+++ 1-classes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using _1_classes;
 /*
 ---------- Sem ORIETAÇÃO A OBJETOS ----------
 double xA, xB, xC, yA, yB, yC;
@@ -31,3 +32,43 @@ if (areaX > areaY)
     System.Console.WriteLine("Maior área: Y");
 }
 */
+
+ //* ---------- Com classes ---------- *//
+Triangulo x, y;
+
+x = new Triangulo();
+y = new Triangulo();
+
+
+Console.WriteLine("Entre com as medidas da triângulo X:");
+
+x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+Console.WriteLine("Entre com as medidas da triângulo Y:");
+y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double p = (x.A + x.B + x.C) / 2.0;
+double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+p = (y.A + y.B + y.C) / 2.0;
+double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+Console.WriteLine($"Área de X =  {areaX.ToString("F4", CultureInfo.InvariantCulture)}");
+Console.WriteLine($"Área de Y =  {areaY.ToString("F4", CultureInfo.InvariantCulture)}");
+
+if (areaX > areaY)
+{
+    Console.WriteLine("A maior área: X");
+}
+else
+{
+    System.Console.WriteLine("Maior área: Y");
+}
+
+
+
---
+++ 1-classes/Triangulo.cs
@@ -0,0 +1,9 @@
+namespace _1_classes
+{
+    public class Triangulo
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+    }
+}
